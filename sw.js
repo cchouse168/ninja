ce57055dc483a9a7d52729365c06cbc1aef2f9b0
cchouse168 <wh0831.chiu@gmail.com>
2025-11-20 23:27:52 +0800

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,23 @@
+const CACHE_NAME = 'pixel-ninja-v1';
+const ASSETS = [
+    './index.html',
+    './manifest.json',
+    './icon.png',
+    'https://cdnjs.cloudflare.com/ajax/libs/tone/14.8.49/Tone.min.js'
+];
+
+self.addEventListener('install', (e) => {
+    e.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => {
+            return cache.addAll(ASSETS);
+        })
+    );
+});
+
+self.addEventListener('fetch', (e) => {
+    e.respondWith(
+        caches.match(e.request).then((response) => {
+            return response || fetch(e.request);
+        })
+    );
+});
